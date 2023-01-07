246a5643acbda81bf138006d2200dd695d38b823
Oz Hayun <ozhayun2@gmail.com>
2023-01-07 22:58:38 +0200

    solved perform operation log

--- Stack.py
+++ Stack.py
@@ -29,8 +29,9 @@ def add_arguments(stack, put_body):
     stack_logger.setLevel(logging.DEBUG)
     stack_logger.info("Adding total of %d argument(s) to the stack | Stack size: %d", len(args), len(stack))
     stack_logger.debug("Adding arguments: " + ','.join(str(a) for a in args) + " |"
-                       " Stack size before %d |"
-                       " stack size after %d", stack_size_before, len(stack))
+                                                                               " Stack size before %d |"
+                                                                               " stack size after %d",
+                       stack_size_before, len(stack))
     stack_logger.setLevel(logging.INFO)
 
     return Response(hr.convert_to_json(len(stack), None), 200)
@@ -56,29 +57,70 @@ def handle_stack_binary_op(stack, operation, is_valid_op):
     if len(stack) >= 2:
         x = stack.pop()
         y = stack.pop()
-        if operation == 'divide' and y == 0:  # Divide by zero is illegal
-            return Response(hr.convert_to_json(None, "Error while performing operation Divide: division by 0"),
-                            status=409)
-        return Response(hr.convert_to_json(Calculator.calc_binary_op(operation, x, y), None), status=200)
+
+        # Divide by zero is illegal
+        if operation == 'divide' and y == 0:
+            return divide_by_zero()
+
+        # Make operation
+        val = Calculator.calc_binary_op(operation, x, y)
+
+        # stack_logger.setLevel(logging.DEBUG)
+        stack_logger.info("Performing operation %s. Result is %d |"
+                          " stack size: %d", operation, val, len(stack))
+        stack_logger.debug("Performing operation: %s(%d,%d) = %d", operation, x, y, val)
+        # stack_logger.setLevel(logging.INFO)
+        return Response(hr.convert_to_json(val, None), status=200)
+
     # Not enough arguments
     else:
-        return Response(hr.convert_to_json(None, "Error: cannot implement operation " + str(operation) +
-                                           ". It requires " + str(is_valid_op) + " arguments and the stack has only " +
-                                           str(len(stack)) + " arguments"), status=409)
+        return not_enough_args(stack, operation, is_valid_op)
+
+
+def divide_by_zero():
+    stack_logger.error("Server encountered an error ! message:"
+                       " Error while performing operation Divide: division by 0")
+    return Response(hr.convert_to_json(None, "Error while performing operation Divide: division by 0"),
+                    status=409)
+
+
+def not_enough_args(stack, operation, is_valid_op):
+    stack_logger.error("Server encountered an error ! message: "
+                       "Error: cannot implement operation " + str(operation) +
+                       ". It requires " + str(is_valid_op) +
+                       " arguments and the stack has only " +
+                       str(len(stack)) + " arguments")
+    return Response(hr.convert_to_json(None, "Error: cannot implement operation " + str(operation) +
+                                       ". It requires " + str(is_valid_op) + " arguments and the stack has only " +
+                                       str(len(stack)) + " arguments"), status=409)
 
 
 def handle_stack_unary_op(stack, operation, is_valid_op):
     if len(stack) >= 1:
         x = stack.pop()
         if x < 0 and operation == 'fact':
-            return Response(hr.convert_to_json(None, "Error while performing operation Factorial: not supported for "
-                                                     "the negative number"), status=409)
-        return Response(hr.convert_to_json(Calculator.calc_unary_op(operation, x), None), status=200)
+            return negative_fact()
+
+        # Make operation
+        val = Calculator.calc_unary_op(operation, x)
+
+        stack_logger.info("Performing operation %s. Result is %d |"
+                          " stack size: %d", operation, val, len(stack))
+        stack_logger.debug("Performing operation: %s(%d) = %d", operation, x, val)
+
+        return Response(hr.convert_to_json(val, None), status=200)
+
+
     # Not enough arguments
     else:
-        return Response(hr.convert_to_json(None, "Error: cannot implement operation " + str(operation) +
-                                           ". It requires " + str(is_valid_op) + " arguments and the stack has only " +
-                                           str(len(stack)) + " arguments"), status=409)
+        return not_enough_args(stack, operation, is_valid_op)
+
+
+def negative_fact():
+    stack_logger.error("Server encountered an error ! message: Error while performing operation Factorial: not "
+                       "supported for the negative number")
+    return Response(hr.convert_to_json(None, "Error while performing operation Factorial: not supported for "
+                                             "the negative number"), status=409)
 
 
 # This function is for deleting some arguments from stack
