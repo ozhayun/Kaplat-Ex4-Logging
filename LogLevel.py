f340393d5ba397cc90d42ad2af2d8424433b650c
Oz Hayun <ozhayun2@gmail.com>
2023-01-09 12:04:31 +0200

    Fix response- change from json to string as requested

--- LogLevel.py
+++ LogLevel.py
@@ -1,5 +1,4 @@
 import json
-
 import HandleRequest as hr
 from flask import Response
 
@@ -16,31 +15,33 @@ stack_logger = hr.stack_logger
 independent_logger = hr.independent_logger
 
 
+# This function return log level of requested logger as a string
 def get_log_level(logger_name):
     if logger_name == 'request-logger':
-        return Response(json.dumps(levels[request_logger.getEffectiveLevel()]), status=200)
+        return Response(levels[request_logger.getEffectiveLevel()], status=200)
     if logger_name == 'stack-logger':
-        return Response(json.dumps(levels[stack_logger.getEffectiveLevel()]), status=200)
+        return Response(levels[stack_logger.getEffectiveLevel()], status=200)
     if logger_name == 'independent-logger':
-        return Response(json.dumps(levels[independent_logger.getEffectiveLevel()]), status=200)
+        return Response(levels[independent_logger.getEffectiveLevel()], status=200)
     return Response("Logger: %s, not exists", logger_name, status=409)
 
 
+# This function sets the logger to requested level
 def set_log_level(logger_name, logger_level):
     logger_level = logger_level.upper()
     if logger_level in levels.values():
         if logger_name == 'request-logger':
             request_logger.setLevel(logger_level)
-            return Response(json.dumps(logger_level.upper()), status=200)
+            return Response(logger_level.upper(), status=200)
 
         if logger_name == 'stack-logger':
             stack_logger.setLevel(logger_level)
-            return Response(json.dumps(logger_level.upper()), status=200)
+            return Response(logger_level.upper(), status=200)
 
         if logger_name == 'independent-logger':
             independent_logger.setLevel(logger_level)
-            return Response(json.dumps(logger_level.upper()), status=200)
-        return Response(json.dumps("Logger not exists"), status=409)
-    return Response(json.dumps("Log level not exists"), status=409)
+            return Response(logger_level.upper(), status=200)
+        return Response("Logger not exists", status=409)
+    return Response("Log level not exists", status=409)
 
 
